445224c7416e7c6901941555cfef71b299d102b7
Gabriel-Moscone <gabriel@infoservsoftware.com.br>
2021-01-26 09:27:21 -0300

    env

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const COMPRAS_URL = process.env.COLOR;
+
   return (
     <div className="App">
-      <header className="App-header">teste</header>
+      <header className="App-header">{COMPRAS_URL}</header>
     </div>
   );
 }
