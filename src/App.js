afda9ae477bf3694a3229f24cbd01dfa3d3ceacb
Gabriel-Moscone <gabriel@infoservsoftware.com.br>
2021-01-26 10:04:33 -0300

    dev

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 function App() {
   const COLOR = process.env.REACT_APP_COLOR;
 
-  console.log(process.env.REACT_APP_COLOR);
-
   return (
     <div className="App">
       <header className="App-header">{COLOR}</header>
